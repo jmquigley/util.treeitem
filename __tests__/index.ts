57ac02653aefe838eae12b2db61f6c3da588c0de
James Quigley <jmquigley@outlook.com>
2019-02-14 10:07:34 -0600

    Converts module to class
    - A class is used to wrap the TreeData so that it is properly sanitized by the contstrutor.
    It is needed because other functiosn rely on valid parent/child key relationships that
    may not be properly set on initial creation.
    - Added a new find method to search the tree by id (breadth search)
    - Added a debug flag to the package.  Used by the util.log output messages
    - Added a test fixture with predicatable keys
    - Added a test fixture for key search

--- __tests__/index.ts
+++ __tests__/index.ts
@@ -1,8 +1,14 @@
 "use strict";
 
+import logger from "util.log";
 import {Fixture} from "util.fixture";
-import {sanitize, TreeItem, walk} from "../index";
+import {TreeData, TreeItem} from "../index";
 
+const pkg = require("../package.json");
+const log = logger.instance({
+	debug: pkg.debug,
+	namespace: "treeitem.test"
+});
 const testing: boolean = true;
 
 test("Test the walk function on a basic TreeItem fixture object", () => {
@@ -10,10 +16,13 @@ test("Test the walk function on a basic TreeItem fixture object", () => {
 	expect(fixture).toBeDefined();
 	expect(fixture.obj).toBeDefined();
 
+	const td = new TreeData(fixture.obj["treeData"]);
+	expect(td).toBeDefined();
+
 	// Walk through the tree.  Concatenate the title values together
 	// to create a string that can be compared for ordering
 	let out: string = "";
-	walk(fixture.obj["treeData"], (it: TreeItem) => {
+	td.walk((it: TreeItem) => {
 		out += `${it.title} `;
 	});
 	out = out.trim();
@@ -27,8 +36,11 @@ test("Test the sanitize function on a basic TreeItem fixture object", () => {
 	expect(fixture).toBeDefined();
 	expect(fixture.obj).toBeDefined();
 
-	walk(fixture.obj["treeData"], (it: TreeItem) => {
-		const newIt = sanitize(it, testing);
+	const td = new TreeData(fixture.obj["treeData"]);
+	expect(td).toBeDefined();
+
+	td.walk((it: TreeItem) => {
+		const newIt = td.sanitize(it, testing);
 
 		expect(newIt).toHaveProperty("id");
 		expect(newIt).toHaveProperty("data");
@@ -37,5 +49,43 @@ test("Test the sanitize function on a basic TreeItem fixture object", () => {
 		expect(newIt).toHaveProperty("subtitle");
 		expect(newIt).toHaveProperty("expanded");
 		expect(newIt).toHaveProperty("children");
-	});
+	}, false); // turn off sanitize
 });
+
+test("Test searching for an id within the tree", () => {
+	const fixture = new Fixture("search");
+	expect(fixture).toBeDefined();
+	expect(fixture.obj).toBeDefined();
+
+	const td = new TreeData(fixture.obj["treeData"]);
+	expect(td).toBeDefined();
+
+	// Item should be found in the tree
+	let it: TreeItem = td.find(4);
+	expect(it).toBeDefined();
+	log.debug("TreeItem -> %O", it);
+
+	expect(it.title).toBe("2.0");
+	expect(it.expanded).toBe(true);
+	expect(it.children.length).toBe(3);
+	expect(it.parent.id).toBeNull();
+
+	// Item should not be found in the tree
+	it = td.find(127);
+	expect(it).toBeNull();
+});
+
+// test("Test assignment of parent/child relationships after walk", () => {
+// 	const fixture = new Fixture("predictable");
+// 	expect(fixture).toBeDefined();
+// 	expect(fixture.obj).toBeDefined();
+//
+// 	let out: string = "";
+// 	walk(fixture.obj["treeData"], (it: TreeItem) => {
+// 		out += `${it.title} `;
+// 	});
+// 	out = out.trim();
+//
+// 	expect(out).toBeDefined();
+// 	expect(out).toBe("1.0 1.1 1.2 2.0 2.1 2.2");
+// });
--- index.ts
+++ index.ts
@@ -1,9 +1,20 @@
 "use strict";
 
-import {getUUID} from "util.toolbox";
+import {nl} from "util.constants";
+import {Queue} from "util.ds";
+import logger from "util.log";
+import {getUUID, nilEvent} from "util.toolbox";
+
+const pkg = require("./package.json");
+const log = logger.instance({
+	debug: pkg.debug,
+	namespace: "treeitem"
+});
+
+export type TreeId = string | number;
 
 export interface TreeItem {
-	id?: string | number;
+	id?: TreeId;
 	data?: any;
 	parent?: TreeItem;
 
@@ -14,90 +25,191 @@ export interface TreeItem {
 }
 
 export type WalkCallback = (val: TreeItem) => void;
+export type ComparatorFn = (it: TreeItem) => boolean;
 
-let sequence: number = 0;
-function getKey(testing: boolean = false): string {
-	if (testing) {
-		return `${sequence++}`;
+const nullParent: TreeItem = {
+	id: null,
+	data: null,
+	parent: null,
+	title: "",
+	subtitle: "",
+	expanded: true,
+	children: []
+};
+
+export class TreeData {
+	private _treeData: TreeItem[];
+
+	constructor(
+		treeData: TreeItem[],
+		private testing: boolean = false,
+		private sequence: number = 0
+	) {
+		this.treeData = treeData;
 	}
 
-	return getUUID();
-}
-
-/**
- * Takes an input node and ensures that it has all possible fields.	 It
- * also creates the node key value if one does not exist.
- * @param node {TreeItem} the node to fix
- * @param testing {boolean} a flag that denotes the module is under testing
- * When testing the keys are generated as a sequence instead of as a UUID
- * @param defaultTitle {string} ('default') a default string set for the
- * title if it doesn't exist or is empty.
- */
-export function sanitize(
-	node: TreeItem,
-	testing: boolean = false,
-	defaultTitle: string = "default"
-): TreeItem {
-	if (!("parent" in node)) {
-		node["parent"] = null;
+	get treeData(): TreeItem[] {
+		return this._treeData;
 	}
 
-	if (!("id" in node) || node["id"] == null) {
-		node["id"] = getKey(testing);
+	set treeData(val: TreeItem[]) {
+		this._treeData = val;
+		this.walk(nilEvent);
 	}
 
-	if (!("title" in node) || node["title"] == null) {
-		node["title"] = defaultTitle;
+	/**
+	 * Performs a breadth search of the tree for a matching id value.
+	 * @param compare {ComparatorFn} compaaison function provided by the specialized
+	 * search routine.
+	 * @param id {string} the id value to search for
+	 * @param treeData {TreeItem[]} the tree struture to search
+	 * @return {TreeItem} of the item found otherwise null
+	 */
+	public find(id: TreeId): TreeItem {
+		const q = new Queue<TreeItem>();
+
+		if (this.treeData == null || this.treeData.length < 1) {
+			log.warn("treeData is empty");
+			return null;
+		}
+
+		let children: TreeItem[] = this.treeData;
+		let item: TreeItem = null;
+
+		do {
+			for (const child of children) {
+				// save the current list into the Q
+				q.enqueue(child);
+			}
+
+			item = q.dequeue();
+			if (item.id === id) {
+				q.drain();
+				return item;
+			} else {
+				children = item.children;
+			}
+		} while (q.size > 0);
+
+		return null; // no item found
 	}
 
-	if (!("subtitle" in node)) {
-		node["subtitle"] = "";
+	/**
+	 * Generates a new unique key for a node.  When the testing flag is set
+	 * to true, then the id is an ordered sequence.  This is done to make
+	 * the keys predictable when the code is under test.
+	 * @return {string} the new key value.
+	 */
+	private getNewKey(): string {
+		if (this.testing) {
+			log.debug(`Creating testing key with sequence: ${this.sequence}`);
+			return `${this.sequence++}`;
+		}
+
+		return getUUID();
 	}
 
-	if (!("expanded" in node)) {
-		node["expanded"] = true;
+	/**
+	 * Takes an input node and ensures that it has all possible fields.	 It
+	 * also creates the node key value if one does not exist.
+	 * @param node {TreeItem} the node to fix
+	 * @param testing {boolean} a flag that denotes the module is under testing
+	 * When testing the keys are generated as a sequence instead of as a UUID
+	 * @param defaultTitle {string} ('default') a default string set for the
+	 * title if it doesn't exist or is empty.
+	 */
+	public sanitize(
+		node: TreeItem,
+		defaultTitle: string = "default"
+	): TreeItem {
+		if (!("parent" in node)) {
+			node["parent"] = nullParent;
+		}
+
+		if (!("id" in node) || node["id"] == null) {
+			node["id"] = this.getNewKey();
+		}
+
+		if (!("title" in node) || node["title"] == null) {
+			node["title"] = defaultTitle;
+		}
+
+		if (!("subtitle" in node)) {
+			node["subtitle"] = "";
+		}
+
+		if (!("expanded" in node)) {
+			node["expanded"] = true;
+		}
+
+		if (!("data" in node)) {
+			node["data"] = null;
+		}
+
+		if (!("children" in node)) {
+			node["children"] = [];
+		}
+
+		return node;
 	}
 
-	if (!("data" in node)) {
-		node["data"] = null;
+	/**
+	 * Prints the internals of the current tree.  It will print the id, the
+	 * parent id, and the title for each node.
+	 * @param treeData {TreeItem[]} the tree data structure to create as a
+	 * string
+	 * @return {string} a string representing the tree structure.
+	 */
+	public toString(): string {
+		let s: string = nl;
+
+		this.walk((it: TreeItem) => {
+			let parentId: TreeId = "";
+			if (it.parent != null) {
+				parentId = it.parent.id;
+			}
+
+			s += `title: ${it.title}, id: ${
+				it.id
+			}, parent.id: ${parentId}${nl}`;
+		});
+
+		return s;
 	}
 
-	if (!("children" in node)) {
-		node["children"] = [];
-	}
+	/**
+	 * Performs an inorder traversal of the current tree.  At each node
+	 * a callback function is executed and the node being processed
+	 * is given as a parameter.
+	 * @param fn {WalkCallback} a callback function invoked on each
+	 * node as it is encountered.
+	 * @param usesanitize {boolean} (true) if true it validates the
+	 * contents of each TreeItem encountered
+	 * @return {TreeeItem[]} a reference to the tree structure that was
+	 * processed.
+	 */
+	public walk(fn: WalkCallback, usesanitize: boolean = true): TreeItem[] {
+		if (typeof fn !== "function") {
+			throw new Error("walk() parameter must be a function");
+		}
 
-	return node;
-}
+		const self: any = this;
 
-/**
- * Performs an inorder traversal of the current tree.  At each node
- * a callback function is executed and the node being processed
- * is given as a parameter.
- * @param fn {WalkCallback} a callback function invoked on each
- * node as it is encountered.
- */
-export function walk(
-	treeData: TreeItem[],
-	fn: WalkCallback,
-	testing: boolean = false
-) {
-	if (typeof fn !== "function") {
-		throw new Error("walk() parameter must be a function");
-	}
+		function preorder(arr: TreeItem[]) {
+			for (const it of arr) {
+				fn(usesanitize ? self.sanitize(it) : it);
 
-	function preorder(arr: TreeItem[]) {
-		for (const it of arr) {
-			fn(sanitize(it, testing));
+				if ("children" in it && it.children.length > 0) {
+					for (const child of it.children) {
+						child["parent"] = it;
+					}
 
-			if ("children" in it && it.children.length > 0) {
-				for (const child of it.children) {
-					child["parent"] = it;
+					preorder(it.children);
 				}
-
-				preorder(it.children);
 			}
 		}
-	}
 
-	preorder(treeData);
+		preorder(this.treeData);
+		return this.treeData;
+	}
 }
